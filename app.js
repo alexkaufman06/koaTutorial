d71d7645aea7ac7cf4f1ded3df118dbd03737403
Kaufman <alexkaufman06@gmail.com>
2019-11-03 13:18:58 -0800

    Include add thing functionality

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const app = new Koa();
 const path = require('path')
 const router = new KoaRouter();
 const render = require('koa-ejs');
+const bodyParser = require('koa-bodyparser');
 const things = ['My family', 'Programming', 'Music']  // This would be replace with a real DB
 
-// app.use(json());
+// app.use(json()); // Renders JSON with formatting
+app.use(bodyParser());
+
+app.context.user = "Brad";
 
 render(app, {
   root: path.join(__dirname, 'views'),
@@ -17,14 +21,29 @@ render(app, {
   debug: false
 });
 
-router.get('/', async ctx => {
+router.get('/', index);
+router.get('/add', showAdd);
+router.post('/add', add);
+
+async function index(ctx) {
   await ctx.render('index', {
     title: 'Things/Stuffs I Love:',
     things: things
   });
-});
+}
 
-router.get('/test', ctx => (ctx.body = "Hello Test"));
+async function showAdd(ctx) {
+  await ctx.render('add');
+}
+
+async function add(ctx) {
+  const body = ctx.request.body;
+  things.push(body.thing);
+  ctx.redirect('/');
+}
+
+router.get('/test', ctx => (ctx.body = `Hello ${ctx.user}`));
+router.get('/test2/:name', ctx => (ctx.body = `Hello ${ctx.params.name}`));
 
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(3000, () => console.log('Server started at: http://localhost:3000/'));
